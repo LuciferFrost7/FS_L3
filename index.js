56ed99de544fa27b991393b83adc09f51ee6eceb
LuciferFrost7 <lucifervanfrost@gmail.com>
2025-02-27 11:29:09 +0300

    create fileRead function

--- index.js
+++ index.js
@@ -10,5 +10,15 @@ function fileWrite($fileName, $message){
     });
 }
 
+// fileWrite('src/files/output_file_z1.txt', '1 функция записи в файл сообщения');
 
-fileWrite('src/files/output_file_z1.txt', '1 функция записи в файл сообщения');
+function fileRead($fileName){
+    $fs.readFile($fileName, 'utf8', function ($error, $text) {
+        if($error){
+            return console.log('Произошла ошибка! Видимо выбранного файла не существует! =^(');
+        }
+        console.log(`Результатом прочтения файла стало:\n${$text.toString()}`);
+    })
+}
+
+fileRead('src/files/input_file_z2.txt');
